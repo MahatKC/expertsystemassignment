312839011939e74f5e7242a31e946cbbc3dd836d
MahatKC <mkarvat@gmail.com>
2021-05-25 12:08:07 -0300

    novas regras e fatos

--- main.py
+++ main.py
@@ -2,69 +2,43 @@ from random import choice
 import schema
 from experta import *
 
-class Demanda(Fact):
-    kwh_demand = Field(lambda kwh_demand: isinstance(kwh_demand, int) and kwh_demand>=0, mandatory=True)
-
-class Classe_Consumo(Fact):
-    #0 -> residencial
-    #1 -> rural
-    #2 -> industrial
-    classe_consumo = Field(lambda classe_consumo: 
-                        isinstance(classe_consumo, int) and classe_consumo in (0,1,2), mandatory=True)
-
-class Radiacao(Fact):
-    radiacao = Field(int, mandatory=True)
-
-class Longitude(Fact):
-    longitude = Field(lambda longitude:
+class Solar(Fact):
+    latitude = Field(lambda longitude:
                         isinstance(longitude, float) and longitude>=-90 and longitude<=90, mandatory=True)
-
-class Altitude(Fact):
     altitude = Field(lambda altitude:
                         isinstance(altitude, int) and altitude>=-500 and altitude<=9000, mandatory=True)
 
-class Area_Disponivel(Fact):
-    area = Field(lambda area: isinstance(area, int) and area>=0, mandatory=True)
+class Geotermica(Fact):
+    temperatura_subtarranea = Field(float, mandatory=True)
 
-class Disponbilidade_Hidrica(Fact):
-    disponibilidade = Field(int, mandatory=True)
+class Maremotriz(Fact):
+    diferenca_mare = Field(lambda diferenca_mare:
+                            isinstance(diferenca_mare, float) and diferenca_mare>=0) #valor em metros
+    proximidade_mar = Field(float, mandatory=True) #valor em quilometros
 
-class Temperatura_Subterranea(Fact):
-    temperatura = Field(float, mandatory=True)
+class Eolica(Fact):
+    velocidade_vento = Field(lambda velocidade_vento: isinstance(velocidade_vento, float)
+                                and velocidade_vento>=0, mandatory=True)
 
-class Fontes_Quentes(Fact):
-    fontes_quentes = Field(bool, mandatory=True)
-"""
-class RobotCrossStreet(KnowledgeEngine):
-    @Rule(Light(color='green'))
-    def green_light(self):
-        print("Walk")
+class Hidrica(Fact):
+    parametro1 = Field(int, mandatory=True)
 
-    @Rule(Light(color='red'))
-    def red_light(self):
-        print("BAum")
-
-    @Rule(AS.light << Light(color=L('yellow') | L('blinking-yellow')))
-    def cautious(self, light):
-        print("Be cautious because light is", light["color"])
-"""
 class AnaliseViabilidade(KnowledgeEngine):
-    @Rule(Demanda(kwh_demand=P(lambda x: x>500)),
-            Classe_Consumo(classe_consumo=L(0)|L(1)),
-            Radiacao(radiacao=P(lambda y: y>5)))
-    def pikachu(self):
-        print("vai curintcha")
+    @Rule(Maremotriz(diferenca_mare=P(lambda d: d>=7),proximidade_mar=P(lambda p: p<=2)))
+    def regra_maremotriz(self):
+        print("Maremotriz é top")
 
-    @Rule(Demanda(kwh_demand=P(lambda x: x<500)),
-            Classe_Consumo(classe_consumo=L(0)|L(1)),
-            Radiacao(radiacao=P(lambda y: y>5)))
-    def charmander(self):
-        print("vai parmera")
+    @Rule(Eolica(velocidade_vento=P(lambda v: v>25)))
+    def regra_eolica(self):
+        print("Eolica é top")
 
+    @Rule(Solar(altitude=P(lambda h: h<4000), latitude=P(lambda y: abs(y)<45)))
+    def regra_solar(self):
+        print("Solar é top")
 
 engine = AnaliseViabilidade()
 engine.reset()
-engine.declare(Demanda(kwh_demand=15))
-engine.declare(Classe_Consumo(classe_consumo=0))
-engine.declare(Radiacao(radiacao=13))
+engine.declare(Maremotriz(diferenca_mare=8.0,proximidade_mar=0.08))
+engine.declare(Eolica(velocidade_vento=30.0))
+engine.declare(Solar(altitude=1000, latitude=-10.0))
 engine.run()
