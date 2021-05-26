a2c81183917c6699922c17539c35a8f1a90d1399
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-05-26 11:56:06 -0300

    alinhamentos e ajudaLatitude

--- interface.py
+++ interface.py
@@ -1,19 +1,20 @@
 from tkinter.constants import CENTER
 from PySimpleGUI import PySimpleGUI as sg
 
+ajudaLatitude = 'Este atributo pode ser obtido via Google Maps, \nclicando com o botão direito na coordenada.\nO primeiro valor apresentado refere-se à latitude!'
 sg.theme('DarkBlue2')
 layout = [
-    [sg.Text('Demanda em KW do local'), sg.Input(key='kw', text_color='#DADA3E', size=(19, 1)),
-    sg.Text('Proximidade com o mar (Km) '), sg.Input(key='proximidadeMar', text_color='#DADA3E', size=(10, 1))],
-    [sg.Text('Diferença entre maré alta e baixa (m)'), sg.Input(key='desnivel', text_color='#DADA3E', size=(10, 1)),
-    sg.Text('Velocidade do vento (Km/h)'), sg.Input(key='velocidadeVento', text_color='#DADA3E', size=(12, 1))],
-    [sg.Text('Latitude '), sg.Input(key='latitude', text_color='#DADA3E', size=(11, 1)),
-    sg.Text('Altitude  '), sg.Input(key='altitude', text_color='#DADA3E', size=(11, 1)),
-    sg.Text('Área disponível (m2)'), sg.Input(key='area', text_color='#DADA3E', size=(18, 1))],
-    [sg.Text('Disponibilidade hídrica'), sg.Input(key='dispHidrica', text_color='#DADA3E')],
-    [sg.Text('Temperatura no interior da terra (°C'), sg.Input(key='temperaturaInterna', text_color='#DADA3E')],
-    [sg.Button('   Enviar dados   ', button_color='#DADA3E')]
+    [sg.Text('Proximidade com o mar (Km)           '), sg.Input(key='proximidadeMar', text_color='#DADA3E', size=(15, None))],
+    [sg.Text('Diferença entre maré alta e baixa (m)'), sg.Input(key='desnivel', text_color='#DADA3E', size=(15, None))],
+    [sg.Text('Velocidade do vento (Km/h)              '), sg.Input(key='velocidadeVento', text_color='#DADA3E', size=(15, None))],
+    [sg.Text('Latitude (notação decimal)'), 
+    sg.Image('help-circle.png', tooltip=ajudaLatitude),
+    sg.Input(key='latitude', text_color='#DADA3E', size=(15, None))],
+    [sg.Text('Área para reservatório (m2)              '), sg.Input(key='area', text_color='#DADA3E', size=(15, None))],
+    [sg.Text('Temperatura no interior da terra (°C) '), sg.Input(key='temperaturaInterna', text_color='#DADA3E', size=(15, None))],
+    [sg.Button('Enviar dados', pad=(100, 0), button_color='#DADA3E', size=(18, None))]
 ]
+
 
 janela = sg.Window('Implantação de fontes energéticas', layout)
 
@@ -23,3 +24,4 @@ while True:
         break
     if eventos == 'Enviar dados':
         print(valores)
+        break
