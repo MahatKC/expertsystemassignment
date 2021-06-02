030d84e27a3c23ac6d70e5c8aad9768b5a8cd98b
EnergyFall266 <leonardo.vanzin@hotmail.com>
2021-06-02 11:02:20 -0300

    imagens

--- interface2.py
+++ interface2.py
@@ -1,16 +1,30 @@
-from tkinter.constants import BOTTOM
+from tkinter.constants import BOTTOM, CENTER
 from webbrowser import BackgroundBrowser
 from PySimpleGUI import PySimpleGUI as sg
 
 sg.theme('DarkBlue2')
 layout=[
+    [sg.Text('   As seguintes fontes energéticas são viáveis para a sua propriedade:', font=('arial',16))],
      [sg.Frame(layout=[   
          [sg.Text('   ',background_color='#355167')],   
          [sg.Text('   ',background_color='#355167'),sg.Image('eolica.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Eólica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM), sg.Frame(layout=[   
+     ], title='Eólica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)), 
+     sg.Frame(layout=[   
          [sg.Text('   ',background_color='#355167')],   
-         [sg.Text('   ',background_color='#355167'),sg.Image('eolica.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Eólica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM)]
+         [sg.Text('   ',background_color='#355167'),sg.Image('geotermica.png'),sg.Text('   ',background_color='#355167')]
+     ], title='Geotérmica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
+     [sg.Frame(layout=[   
+         [sg.Text('   ',background_color='#355167')],   
+         [sg.Text('   ',background_color='#355167'),sg.Image('fotovoltaica.png'),sg.Text('   ',background_color='#355167')]
+     ], title='Fotovoltaica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,10)), 
+     sg.Frame(layout=[   
+         [sg.Text('   ',background_color='#355167')],   
+         [sg.Text('   ',background_color='#355167'),sg.Image('hidreletrica.png'),sg.Text('   ',background_color='#355167')]
+     ], title='Hídrica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,10))],
+     [sg.Frame(layout=[   
+         [sg.Text('   ',background_color='#355167')],   
+         [sg.Text('   ',background_color='#355167'),sg.Image('maremotriz.png'),sg.Text('   ',background_color='#355167')]
+     ], title='Maremotriz\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(181,10))]
 
 
 ]
