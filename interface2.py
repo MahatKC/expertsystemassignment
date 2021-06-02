916202d7e8b510d55ea0a8f75b3af0c4c4180907
MahatKC <mkarvat@gmail.com>
2021-06-02 12:05:04 -0300

    if else interface

--- interface2.py
+++ interface2.py
@@ -12,39 +12,66 @@ else:
     maremotriz_bg='#A9A9A9'
     maremotriz_title_color='#000000'
 
-if backend.viabilidade['maremotriz']:
-    maremotriz_img='maremotriz.png'
-    maremotriz_bg='#355167'
-    maremotriz_title_color='#DADA3E'
+if backend.viabilidade['eolica']:
+    eolica_img='eolica.png'
+    eolica_bg='#355167'
+    eolica_title_color='#DADA3E'
 else:
-    maremotriz_img='maremotrizbw.png'
-    maremotriz_bg='#A9A9A9'
-    maremotriz_title_color='#000000'
+    eolica_img='eolicabw.png'
+    eolica_bg='#A9A9A9'
+    eolica_title_color='#000000'
+
+if backend.viabilidade['solar']:
+    fotovoltaica_img='fotovoltaica.png'
+    fotovoltaica_bg='#355167'
+    fotovoltaica_title_color='#DADA3E'
+else:
+    fotovoltaica_img='fotovoltaicabw.png'
+    fotovoltaica_bg='#A9A9A9'
+    fotovoltaica_title_color='#000000'
+
+if backend.viabilidade['geotermica']:
+    geotermica_img='geotermica.png'
+    geotermica_bg='#355167'
+    geotermica_title_color='#DADA3E'
+else:
+    geotermica_img='geotermicabw.png'
+    geotermica_bg='#A9A9A9'
+    geotermica_title_color='#000000'
+
+if backend.viabilidade['hidrica']:
+    hidreletrica_img='hidreletrica.png'
+    hidreletrica_bg='#355167'
+    hidreletrica_title_color='#DADA3E'
+else:
+    hidreletrica_img='hidreletricabw.png'
+    hidreletrica_bg='#A9A9A9'
+    hidreletrica_title_color='#000000'
 
 
 sg.theme('DarkBlue2')
 layout=[
     [sg.Text('                       Abaixo, as fontes energéticas em destaque são viáveis para a sua propriedade:', font=('arial',16))],
      [sg.Frame(layout=[   
-         [sg.Text('   ',background_color='#A9A9A9')],   
-         [sg.Text('   ',background_color='#A9A9A9'),sg.Image('eolicabw.png'),sg.Text('   ',background_color='#A9A9A9')]
-     ], title='Eólica\n',title_color='#000000', font=('arial',20),border_width=0, background_color='#A9A9A9', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)), 
+         [sg.Text('   ',background_color=eolica_bg)],   
+         [sg.Text('   ',background_color=eolica_bg),sg.Image(eolica_img),sg.Text('   ',background_color=eolica_bg)]
+     ], title='Eólica\n',title_color=eolica_title_color, font=('arial',20),border_width=0, background_color=eolica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)), 
      sg.Frame(layout=[   
-         [sg.Text('   ',background_color='#355167')],   
-         [sg.Text('   ',background_color='#355167'),sg.Image('geotermica.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Geotérmica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)),
+         [sg.Text('   ',background_color=geotermica_bg)],   
+         [sg.Text('   ',background_color=geotermica_bg),sg.Image(geotermica_img),sg.Text('   ',background_color=geotermica_bg)]
+     ], title='Geotérmica\n',title_color=geotermica_title_color, font=('arial',20),border_width=0, background_color=geotermica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)),
      sg.Frame(layout=[   
-         [sg.Text('   ',background_color='#355167')],   
-         [sg.Text('   ',background_color='#355167'),sg.Image('maremotriz.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Maremotriz\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
+         [sg.Text('   ',background_color=maremotriz_bg)],   
+         [sg.Text('   ',background_color=maremotriz_bg),sg.Image(maremotriz_img),sg.Text('   ',background_color=maremotriz_bg)]
+     ], title='Maremotriz\n',title_color=maremotriz_title_color, font=('arial',20),border_width=0, background_color=maremotriz_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
      [sg.Text('                                        '), sg.Frame(layout=[   
-         [sg.Text('   ',background_color='#355167')],   
-         [sg.Text('   ',background_color='#355167'),sg.Image('fotovoltaica.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Fotovoltaica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
+         [sg.Text('   ',background_color=fotovoltaica_bg)],   
+         [sg.Text('   ',background_color=fotovoltaica_bg),sg.Image(fotovoltaica_img),sg.Text('   ',background_color=fotovoltaica_bg)]
+     ], title='Fotovoltaica\n',title_color=fotovoltaica_title_color, font=('arial',20),border_width=0, background_color=fotovoltaica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
      sg.Frame(layout=[   
-         [sg.Text('   ',background_color='#355167')],   
-         [sg.Text('   ',background_color='#355167'),sg.Image('hidreletrica.png'),sg.Text('   ',background_color='#355167')]
-     ], title='Hídrica\n',title_color='#DADA3E', font=('arial',20),border_width=0, background_color='#355167', title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
+         [sg.Text('   ',background_color=hidreletrica_bg)],   
+         [sg.Text('   ',background_color=hidreletrica_bg),sg.Image(hidreletrica_img),sg.Text('   ',background_color=hidreletrica_bg)]
+     ], title='Hídrica\n',title_color=hidreletrica_title_color, font=('arial',20),border_width=0, background_color=hidreletrica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
 ]
 
 janela = sg.Window('Implantação de Fontes Energéticas', layout, size=(1050,680), location=(150,10)) #titulo do janela
