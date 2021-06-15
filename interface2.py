4db94390c9b7651d46b6a6e6a69c35e93aafcf31
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-06-15 18:46:29 -0300

    espaço text

--- interface2.py
+++ interface2.py
@@ -20,11 +20,11 @@ else:
     eolica_img='eolicabw.png'
     eolica_bg='#A9A9A9'
     eolica_title_color='#000000'
-# A energia fotovoltaica sempre é viavel
+
 fotovoltaica_img='fotovoltaica.png'
 fotovoltaica_bg='#355167'
 fotovoltaica_title_color='#DADA3E'
-popupSolar = False # porem pode ocorrer de ela nao ter rentabilidade otima, neste caso é exibido uma mensagem ao usuario
+popupSolar = False
 if not backend.viabilidade['solar']:
     popupSolar = True
 
@@ -46,9 +46,9 @@ else:
     hidreletrica_bg='#A9A9A9'
     hidreletrica_title_color='#000000'
 
-popText = 'Na presente condição, fontes fotovoltaicas não apresentam\nrentabilidade ótima, mas ainda são viáveis' # texto utilizado na mensagem warning
+popText = 'Na presente condição, fontes fotovoltaicas não apresentam\nrentabilidade ótima, mas ainda são viáveis'
 sg.theme('DarkBlue2')
-layout=[ # layout exibido quando a energia fotovoltaica nao tem rentabilidade otima
+layout=[
     [sg.Text('                       Abaixo, as fontes energéticas em destaque são viáveis para a sua propriedade:', font=('arial',16))], 
      [sg.Frame(layout=[   #imagem e nome da fonte energetica
          [sg.Text('   ',background_color=eolica_bg)],   #pega as informações do if e else que serao exibidas
@@ -63,7 +63,7 @@ layout=[ # layout exibido quando a energia fotovoltaica nao tem rentabilidade ot
          [sg.Text('   ',background_color=maremotriz_bg),sg.Image(maremotriz_img),sg.Text('   ',background_color=maremotriz_bg)]
      ], title='Maremotriz\n',title_color=maremotriz_title_color, font=('arial',20),border_width=0, background_color=maremotriz_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
      [sg.Text('                                        '), sg.Frame(layout=[   #imagem e nome da fonte energetica / espaços em branco para alocar melhor a imgem na tela
-         [sg.Text('                                                                    ',background_color=fotovoltaica_bg),  sg.Image('warning.png', tooltip=popText, background_color=fotovoltaica_bg)], # mensagem de aviso
+         [sg.Text('                                                                    ',background_color=fotovoltaica_bg),  sg.Image('warning.png', tooltip=popText, background_color=fotovoltaica_bg)],
          [sg.Text('   ',background_color=fotovoltaica_bg),sg.Image(fotovoltaica_img),sg.Text('   ',background_color=fotovoltaica_bg)]
      ], title='Fotovoltaica\n',title_color=fotovoltaica_title_color, font=('arial',20),border_width=0, background_color=fotovoltaica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
      sg.Frame(layout=[   #imagem e nome da fonte energetica
@@ -72,7 +72,7 @@ layout=[ # layout exibido quando a energia fotovoltaica nao tem rentabilidade ot
      ], title='Hídrica\n',title_color=hidreletrica_title_color, font=('arial',20),border_width=0, background_color=hidreletrica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
 ]
 
-layout2=[ # layout exibido quando a energia fotovoltaica possui rentabilidade otima
+layout2=[
     [sg.Text('                       Abaixo, as fontes energéticas em destaque são viáveis para a sua propriedade:', font=('arial',16))], 
      [sg.Frame(layout=[   #imagem e nome da fonte energetica
          [sg.Text('   ',background_color=eolica_bg)],   #pega as informações do if e else que serao exibidas
@@ -87,7 +87,7 @@ layout2=[ # layout exibido quando a energia fotovoltaica possui rentabilidade ot
          [sg.Text('   ',background_color=maremotriz_bg),sg.Image(maremotriz_img),sg.Text('   ',background_color=maremotriz_bg)]
      ], title='Maremotriz\n',title_color=maremotriz_title_color, font=('arial',20),border_width=0, background_color=maremotriz_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
      [sg.Text('                                        '), sg.Frame(layout=[   #imagem e nome da fonte energetica / espaços em branco para alocar melhor a imgem na tela
-         [sg.Text('                                                                    ',background_color=fotovoltaica_bg)],
+         [sg.Text('   ',background_color=fotovoltaica_bg)],
          [sg.Text('   ',background_color=fotovoltaica_bg),sg.Image(fotovoltaica_img),sg.Text('   ',background_color=fotovoltaica_bg)]
      ], title='Fotovoltaica\n',title_color=fotovoltaica_title_color, font=('arial',20),border_width=0, background_color=fotovoltaica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
      sg.Frame(layout=[   #imagem e nome da fonte energetica
@@ -96,10 +96,11 @@ layout2=[ # layout exibido quando a energia fotovoltaica possui rentabilidade ot
      ], title='Hídrica\n',title_color=hidreletrica_title_color, font=('arial',20),border_width=0, background_color=hidreletrica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
 ]
 
-if popupSolar: # seleciona qual layout sera exibido
+if popupSolar:
     janela = sg.Window('Implantação de Fontes Energéticas', layout, size=(1050,680), location=(150,10)) #titulo do janela
 else:
     janela = sg.Window('Implantação de Fontes Energéticas', layout2, size=(1050,680), location=(150,10)) #titulo do janela
+#janela.BringToFront()
 
 while True:
     eventos, valores = janela.read()
