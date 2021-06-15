bf0f6e838bac77dbccc4b12b5da70b649d75cb7f
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-06-15 18:28:50 -0300

    popup

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ layout = [   #definicao da aparencia da interface junto com o conteudo que sera 
     [sg.Text('Diferença entre maré alta e baixa (m)'), sg.Input(key='desnivel', text_color='#DADA3E', size=(15, None))],
     [sg.Text('Velocidade do vento (Km/h)              '), sg.Input(key='velocidadeVento', text_color='#DADA3E', size=(15, None))],
     [sg.Text('Latitude (notação decimal)'), 
-    sg.Image('help-circle.png', tooltip=ajudaLatitude), # botao de interrogacao que aparece uma mensagem ao posicionar o mouse em cima
+    sg.Image('help-circle.png', tooltip=ajudaLatitude), # botao de interrogacao que aparece uma mensagem ao deixar o mouse em cima
     sg.Text('      '), sg.Input(key='latitude', text_color='#DADA3E', size=(15, None))],
     [sg.Text('Área para reservatório (m2)               '), sg.Input(key='area', text_color='#DADA3E', size=(15, None))],
     [sg.Text('Temperatura no interior da terra (°C)  '), sg.Input(key='temperaturaInterna', text_color='#DADA3E', size=(15, None))],
@@ -17,12 +17,12 @@ layout = [   #definicao da aparencia da interface junto com o conteudo que sera 
     [sg.Button('Enviar dados', pad=(100, 0), button_color='#DADA3E', size=(18, None))]
 ]
 
-
 janela = sg.Window('Implantação de Fontes Energéticas', layout) #titulo do janela
 
 while True:
     eventos, valores = janela.read()
-    if eventos == sg.WINDOW_CLOSED: # verifica se foi clicado no X superior a direita e caso for fecha a interface
+    if eventos == sg.WINDOW_CLOSED: #verifica se foi clicado no X superior a direita e caso for fecha a interface
         break
     if eventos == 'Enviar dados': #verifica se foi clicado o botao 'enviar dados' e pega os valores que foram inseridos 
+        janela.close()
         break
--- interface2.py
+++ interface2.py
@@ -24,9 +24,9 @@ else:
 fotovoltaica_img='fotovoltaica.png'
 fotovoltaica_bg='#355167'
 fotovoltaica_title_color='#DADA3E'
-
+popupSolar = False
 if not backend.viabilidade['solar']:
-    #PREPARA O POPUP AQUI
+    popupSolar = True
 
 if backend.viabilidade['geotermica']:
     geotermica_img='geotermica.png'
@@ -46,7 +46,7 @@ else:
     hidreletrica_bg='#A9A9A9'
     hidreletrica_title_color='#000000'
 
-
+popText = 'Na presente condição, fontes fotovoltaicas não apresentam\nrentabilidade ótima, mas ainda são viáveis'
 sg.theme('DarkBlue2')
 layout=[
     [sg.Text('                       Abaixo, as fontes energéticas em destaque são viáveis para a sua propriedade:', font=('arial',16))], 
@@ -63,7 +63,7 @@ layout=[
          [sg.Text('   ',background_color=maremotriz_bg),sg.Image(maremotriz_img),sg.Text('   ',background_color=maremotriz_bg)]
      ], title='Maremotriz\n',title_color=maremotriz_title_color, font=('arial',20),border_width=0, background_color=maremotriz_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
      [sg.Text('                                        '), sg.Frame(layout=[   #imagem e nome da fonte energetica / espaços em branco para alocar melhor a imgem na tela
-         [sg.Text('   ',background_color=fotovoltaica_bg)],   
+         [sg.Text('                                                                    ',background_color=fotovoltaica_bg),  sg.Image('warning.png', tooltip=popText, background_color=fotovoltaica_bg)],
          [sg.Text('   ',background_color=fotovoltaica_bg),sg.Image(fotovoltaica_img),sg.Text('   ',background_color=fotovoltaica_bg)]
      ], title='Fotovoltaica\n',title_color=fotovoltaica_title_color, font=('arial',20),border_width=0, background_color=fotovoltaica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
      sg.Frame(layout=[   #imagem e nome da fonte energetica
@@ -72,12 +72,39 @@ layout=[
      ], title='Hídrica\n',title_color=hidreletrica_title_color, font=('arial',20),border_width=0, background_color=hidreletrica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
 ]
 
-janela = sg.Window('Implantação de Fontes Energéticas', layout, size=(1050,680), location=(150,10)) #titulo do janela
+layout2=[
+    [sg.Text('                       Abaixo, as fontes energéticas em destaque são viáveis para a sua propriedade:', font=('arial',16))], 
+     [sg.Frame(layout=[   #imagem e nome da fonte energetica
+         [sg.Text('   ',background_color=eolica_bg)],   #pega as informações do if e else que serao exibidas
+         [sg.Text('   ',background_color=eolica_bg),sg.Image(eolica_img),sg.Text('   ',background_color=eolica_bg)]
+     ], title='Eólica\n',title_color=eolica_title_color, font=('arial',20),border_width=0, background_color=eolica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)), 
+     sg.Frame(layout=[   #imagem e nome da fonte energetica
+         [sg.Text('   ',background_color=geotermica_bg)],   
+         [sg.Text('   ',background_color=geotermica_bg),sg.Image(geotermica_img),sg.Text('   ',background_color=geotermica_bg)]
+     ], title='Geotérmica\n',title_color=geotermica_title_color, font=('arial',20),border_width=0, background_color=geotermica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20)),
+     sg.Frame(layout=[   #imagem e nome da fonte energetica
+         [sg.Text('   ',background_color=maremotriz_bg)],   
+         [sg.Text('   ',background_color=maremotriz_bg),sg.Image(maremotriz_img),sg.Text('   ',background_color=maremotriz_bg)]
+     ], title='Maremotriz\n',title_color=maremotriz_title_color, font=('arial',20),border_width=0, background_color=maremotriz_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,20))],
+     [sg.Text('                                        '), sg.Frame(layout=[   #imagem e nome da fonte energetica / espaços em branco para alocar melhor a imgem na tela
+         [sg.Text('                                                                    ',background_color=fotovoltaica_bg)],
+         [sg.Text('   ',background_color=fotovoltaica_bg),sg.Image(fotovoltaica_img),sg.Text('   ',background_color=fotovoltaica_bg)]
+     ], title='Fotovoltaica\n',title_color=fotovoltaica_title_color, font=('arial',20),border_width=0, background_color=fotovoltaica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0)), 
+     sg.Frame(layout=[   #imagem e nome da fonte energetica
+         [sg.Text('   ',background_color=hidreletrica_bg)],   
+         [sg.Text('   ',background_color=hidreletrica_bg),sg.Image(hidreletrica_img),sg.Text('   ',background_color=hidreletrica_bg)]
+     ], title='Hídrica\n',title_color=hidreletrica_title_color, font=('arial',20),border_width=0, background_color=hidreletrica_bg, title_location=sg.TITLE_LOCATION_BOTTOM, pad=(10,0))]
+]
+
+if popupSolar:
+    janela = sg.Window('Implantação de Fontes Energéticas', layout, size=(1050,680), location=(150,10)) #titulo do janela
+else:
+    janela = sg.Window('Implantação de Fontes Energéticas', layout2, size=(1050,680), location=(150,10)) #titulo do janela
+#janela.BringToFront()
 
 while True:
     eventos, valores = janela.read()
+
     if eventos == sg.WINDOW_CLOSED: # verifica se foi clicado no X superior a direita e caso for fecha a interface
         break
-    if eventos == 'Enviar dados': #verifica se foi clicado o botao 'enviar dados' e pega os valores que foram inseridos 
-        print(valores)
-        break
+    
